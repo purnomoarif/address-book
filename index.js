b1b2b60a1971229ff75ffdf314b40743178eee07
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-06-20 20:25:26 +0700

    create js and console  log

---
+++ index.js
@@ -0,0 +1,16 @@
+const contact = {
+  id: 1,
+  name: "Arif Purnomo",
+  age: 35,
+  email: "purnomoarifdepok@gmail.com",
+  phone: "+62-856-106-6962",
+  website: "https://arifpurnomo-com.vercel.app/",
+  address: {
+    street: "Cawang III",
+    city: "Jakarta",
+    country: "Indonesia",
+  },
+  isFavorited: true,
+};
+
+console.log(Contact);

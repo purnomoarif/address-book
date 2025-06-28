800a5688874a68d0de75931a13f8304e9ab52394
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-06-28 14:43:35 +0700

    add get contact

--- index.js
+++ index.js
@@ -4,28 +4,60 @@ const contacts = [
     name: "Arif Purnomo",
     phone: "+628561066962",
     email: "purnomoarifdepok@gmail.com",
-    address: "Osaka, Japan",
+    address: {
+      city: "Depok",
+      country: "Indonesia",
+    },
+    birthdate: new Date("1989-07-28"),
   },
   {
     id: 2,
+    name: "Purnomo Arif",
+    phone: "+81 123456",
+    email: "purnomoarifwibu@gmail.com",
+    address: {
+      city: "Osaka",
+      country: "Japan",
+    },
+    birthdate: new Date("1990-01-01"),
+  },
+  {
+    id: 3,
     name: "Dinda Ayu Ratnasari",
     phone: "+6285719214858",
     email: "dindaaratnasari@gmail.com",
-    address: "Jakarta, Indonesia",
+    address: {
+      city: "Jakarta",
+      country: "Indonesia",
+    },
+    birthdate: new Date("1997-10-28"),
   },
 ];
-console.log(contacts);
+
+function getContacts() {
+  console.log(contacts);
+  contacts.forEach((contact) => {
+    console.log(`
+      id: ${contact.id}
+      Name: ${contact.name} 
+      Phone: ${contact.phone}
+      Email: ${contact.email}
+      Address: ${contact.address.city}, ${contact.address.country}
+      Birthdate: ${contact.birthdate.toLocaleDateString()}
+    `);
+  });
+}
 
 function showContacts() {
-  for (let id = 0; id < contacts.length; id++) {
-    const contact = contacts[id];
+  for (let index = 0; index < contacts.length; index++) {
+    const contact = contacts[index];
 
     console.log(`
       id: ${contact.id}
-      name: ${contact.name} 
-      phone: ${contact.phone}
-      email: ${contact.email}
-      address: ${contact.address}
+      Name: ${contact.name} 
+      Phone: ${contact.phone}
+      Email: ${contact.email}
+      Address: ${contact.address.city}, ${contact.address.country}
       `);
   }
 }
